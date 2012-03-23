691c1aba88f2b646397b9f5ab2fcb51da8e75847
Nicholas Poorman <nickpoorman@gmail.com>
2012-03-23 17:20:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oswegocsa::Application.config.secret_token = 'ad4dbf9de8faac6d43ca54e1cf7681ace89addcc8a305a0aac8830054d6bd6ff1179ecef7ef59e72a6db67b22f1f4c1e63d7028346635127f66bba8e04573a15'
