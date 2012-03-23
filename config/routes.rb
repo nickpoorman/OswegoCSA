1da1e035d1b4a5c43a32cde5766829dbce6fffd4
Nicholas Poorman <nickpoorman@gmail.com>
2012-03-23 18:50:12 -0400

    added in flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Oswegocsa::Application.routes.draw do
+  resources :users
+  resources :user_sessions
 
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout
   match 'signup' => 'users#new', :as => :signup
 
-  resources :users
-  resources :user_sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
