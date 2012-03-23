709823c504815657c68353099870f18ba582e538
Nicholas Poorman <nickpoorman@gmail.com>
2012-03-23 18:09:59 -0400

    generated user user_sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Oswegocsa::Application.routes.draw do
+  get "user_sessions/new"
+
+  get "user_sessions/create"
+
+  get "user_sessions/destroy"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
